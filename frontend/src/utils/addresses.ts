63cb331db14a508f11c1ad0d7c217e323e81d777
Luiz <lvj23@gmail.com>
2023-10-22 09:23:20 -0300

    utils

---
+++ frontend/src/utils/addresses.ts
@@ -0,0 +1,15 @@
+export const WhaleFinanceAddress = "0x49EbBc5a9364F25284875A201AA39E657E90c5C6";
+export const ZusdAddress = "0x4F28e4Aa42Cec704c41f7641c60a5a8157B3790e";
+
+export const WEthAddress = "0x9c00d4f278Cb6fa3c4A137F7FedfB514a7655928";
+
+export const SwapRouter = "0x7963c1bd24E4511A0b14bf148F93e2556AFe3C27";
+
+export const scanUrl = "https://testscan.bt.io/#";
+
+export const allowedTokens = {
+    "BTTCUSD": ZusdAddress,
+    "WETH": WEthAddress
+} as {
+    [key: string]: string;
+}
