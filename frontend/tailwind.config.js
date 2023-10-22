58b3088ada874138c25efd2a3274ca8769b350b1
Luiz <lvj23@gmail.com>
2023-10-22 09:18:48 -0300

    begin frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#efd24a",
+        "secondary-color": "#020745",
+        "blue-color": "rgb(17,6,122)",
+      },
+    },
+  },
+  plugins: [],
+}
